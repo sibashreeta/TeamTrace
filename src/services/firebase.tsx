ccca8c75cb3ce180337d16923d2fece292f6d8c0
sibashreeta <149070951+sibashreeta@users.noreply.github.com>
2025-06-27 13:38:28 +0530

    Update firebase.tsx

--- src/services/firebase.tsx
+++ src/services/firebase.tsx
@@ -4,23 +4,19 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+
+
 const firebaseConfig = {
-
-  apiKey: "AIzaSyAC5JZdee_nTxzHewXuiokJa-2wkGZTSBA",
-
-  authDomain: "test-17d72.firebaseapp.com",
-
-  projectId: "test-17d72",
-
-  storageBucket: "test-17d72.firebasestorage.app",
-
-  messagingSenderId: "579550488154",
-
-  appId: "1:579550488154:web:82e02f04adcb7b38539a63",
-
-  measurementId: "G-PJDMM4JKBF"
-
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
+
+
 
 
 // Initialize Firebase
